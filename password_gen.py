82aadb9f2df963e573d5e7298200b4508771f624
Daniel <dannyed784@gmail.com>
2025-04-05 10:14:49 -0400

    password generator rev1

---
+++ password_gen.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+
+# the password needs to include:
+# 2 uppercase letters from A to Z
+# 2 lowercase letters from a to z
+# 2 digits from 0 to 9
+# 2 punctiation signs such as ! ? # etc
+
