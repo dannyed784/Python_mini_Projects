e67834ac59d951f32a298031c2229c98ef87ba4f
Daniel <dannyed784@gmail.com>
2025-04-05 22:44:11 -0400

    password generator rev2

--- password_gen.py
+++ password_gen.py
@@ -1,4 +1,5 @@
 import os
+import random
 os.system("clear")
 
 # the password needs to include:
@@ -7,3 +8,30 @@ os.system("clear")
 # 2 digits from 0 to 9
 # 2 punctiation signs such as ! ? # etc
 
+#Using Python you can easily access ASCII values of character using the ord() function. For
+#instance ord("M") returns 77 and chr(77) returns "M".
+
+#randint(): Returns a random number between the given range
+
+#1.Generate 2 random uppercase letter between A to Z
+uppercase_letter1 = chr(random.randint(65,90)) #first random uppercase letter  
+uppercase_letter2 = chr(random.randint(65,90)) #second random uppercase letter 
+
+#2. Generate 2 random lowercase letter between a to z
+lowercase_letter1 = chr(random.randint(97,122)) #first random lowercase letter
+lowercase_letter2 = chr(random.randint(97,122)) #second random lowercase letter
+
+#3. Generate 2 random digit between 0 to 9
+digit1 = chr(random.randint(48,57))
+digit2 = chr(random.randint(48,57))
+
+# 4. Generate 2 random punctuation sign
+punctuationSign1 = chr(random.randint(33,152))
+punctuationSign2 = chr(random.randint(33,152))
+
+#5. Generate de password 
+password = (uppercase_letter1 + uppercase_letter2 + lowercase_letter1 + lowercase_letter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2)
+password_list = list(password)
+
+print(password)
+print(password_list)
