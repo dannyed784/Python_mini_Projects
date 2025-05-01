cb6b1b4a68cfe78430076e9e880dafff48ed5c5a
Daniel <dannyed784@gmail.com>
2025-04-30 21:33:31 -0400

    list overlap v2

---
+++ overlap.py
@@ -0,0 +1,15 @@
+# LIST OVERLAP COMPREHENSIONS
+#Write a program that returns a list that contains only the elements that are common between
+#the lists(without duplicates)
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def comp(list1,list2):
+   c = [num for num in list1 if num in list1 and num in list2] 
+   return c
+
+print(comp(a,b))
